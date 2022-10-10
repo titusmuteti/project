9b404377c5e41d7dff56d49bd6ecef215ac2b8a2
Titus Mumo <titusmumo@gmail.com>
2022-10-10 19:19:18 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <HashRouter>
+  <HashRouter>
       <App />
-    </HashRouter>
-  </React.StrictMode>
+  </HashRouter>
+
 );
